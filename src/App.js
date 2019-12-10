604d8ba0e61e68af95e80a85832088597c301fb8
unknown <2424509696@qq.com>
2019-12-10 23:02:56 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,127 @@
+<<<<<<< HEAD
+import React from 'react';
+import './App.css';
+//let postContent=['Swimming is fun','Swimming is exhausting'];
+//let postImage 
+
+
+function App() {
+  return (
+    <div className="content"> 
+<li class="css-ye6x8s">
+<div class="css-1cp3ece">
+<div class="css-1l4spti">
+
+<div class="css-79elbk">
+  <span class="css-1dv1kvn">Image</span>
+  <a href="https://imgbb.com/"></a><img src="https://i.ibb.co/SrCqgG3/images.jpg" alt="images" border="0"></img>
+
+<figure class="css-ulz9xo" role="group" aria-label="media" itemprop="associatedMedia" itemid="https://static01.nyt.com/images/2019/10/14/business/14amc/merlin_158375169_b5ff113d-8c81-4257-a65c-e54a49aa89f8-thumbWide.jpg?quality=30&amp;auto=webp" itemtype="http://schema.org/ImageObject" itemscope="">
+<div class="css-79elbk">
+
+
+<figcaption class="css-17ai7jg emkp2hg0" itemprop="caption description"></figcaption>
+<h3 class="css-1u1077h ezz47cd0">
+  <span class="css-15g018">THE SHIFT</span>
+</h3>
+<h2 class="css-1j9dxys e1xfvim30">Kodak's Dubious Blockchain Gamble
+          </h2>
+          <p class="css-1echdzn e1xfvim31">What's a 130-year-old photo company doing dabbling in crptocurrency? Either revolutionizing digital rights management or trying to make a quick buck.
+          </p>
+          
+          <div class="css-1nqbnmb ea5icrr0">
+          <p class="css-1xonkmu">By      
+            <span class="css-1n7hynb">Kevin roose</span>
+            </p>
+            <div class="css-n1vcs8 e1xfvim33">
+        <time class="">Jan. 30, 2018</time>
+      </div></div></div></figure></div></div></div> 
+</li>
+<li class="css-ye6x8s">
+<div class="css-1cp3ece">
+<div class="css-1l4spti">
+
+<div class="css-79elbk">
+<span class="css-1dv1kvn">Image</span>
+<a href="https://ibb.co/ByPsx14"></a><img src="https://i.ibb.co/ZBJTPZH/825213.png" alt="825213" border="0"></img>
+
+<figure class="css-ulz9xo" role="group" aria-label="media" itemprop="associatedMedia" itemid="https://static01.nyt.com/images/2019/10/14/business/14amc/merlin_158375169_b5ff113d-8c81-4257-a65c-e54a49aa89f8-thumbWide.jpg?quality=30&amp;auto=webp" itemtype="http://schema.org/ImageObject" itemscope="">
+<div class="css-79elbk">
+
+
+<figcaption class="css-17ai7jg emkp2hg0" itemprop="caption description"></figcaption>
+<h3 class="css-1u1077h ezz47cd0">
+  <span class="css-15g018"></span>
+</h3>
+<h2 class="css-1j9dxys e1xfvim30">Taiwan Retaliates Against Chinese Airlines, Hampering Lunar New Year Travel
+          </h2>
+          <p class="css-1echdzn e1xfvim31">Taiwan, pushing back over encroachment on Taiwan Strait airspace, may leave thousands without flights home for the holiday.
+          </p>
+          
+          <div class="css-1nqbnmb ea5icrr0">
+          <p class="css-1xonkmu">By      
+            <span class="css-1n7hynb">Chris Horton</span>
+            </p>
+            <div class="css-n1vcs8 e1xfvim33">
+        <time class="">Jan. 30, 2018</time>
+      </div></div></div></figure></div></div></div> 
+</li>
+<li class="css-ye6x8s">
+<div class="css-1cp3ece">
+<div class="css-1l4spti">
+
+<div class="css-79elbk">
+<span class="css-1dv1kvn">Image</span>
+<a href="https://imgbb.com/"></a><img src="https://i.ibb.co/YZxdfW8/image.jpg" alt="image" border="0"></img>
+
+<figure class="css-ulz9xo" role="group" aria-label="media" itemprop="associatedMedia" itemid="https://static01.nyt.com/images/2019/10/14/business/14amc/merlin_158375169_b5ff113d-8c81-4257-a65c-e54a49aa89f8-thumbWide.jpg?quality=30&amp;auto=webp" itemtype="http://schema.org/ImageObject" itemscope="">
+<div class="css-79elbk">
+
+
+<figcaption class="css-17ai7jg emkp2hg0" itemprop="caption description"></figcaption>
+<h3 class="css-1u1077h ezz47cd0">
+  <span class="css-15g018"></span>
+</h3>
+<h2 class="css-1j9dxys e1xfvim30">New Jersey Embraces an idea it Once Rejected: Make Utilities Pay to Emit Carbon
+          </h2>
+          <p class="css-1echdzn e1xfvim31">Democratic governors nationwide are taking steps to taxor price emissions within their own borders, even as Trump dismantles federal climate policy.
+          </p>
+          
+          <div class="css-1nqbnmb ea5icrr0">
+          <p class="css-1xonkmu">By      
+            <span class="css-1n7hynb">Brad Plumer</span>
+            </p>
+            <div class="css-n1vcs8 e1xfvim33">
+        <time class="">Jan. 29, 2018</time>
+      </div></div></div></figure></div></div></div> 
+</li></div>
+
+  );
+}
+
+
+
+export default App;
+=======
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
+>>>>>>> Intial commit for Spicy Veggie
---
+++ src/App.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+<<<<<<< HEAD
+  ReactDOM.unmountComponentAtNode(div);
+=======
+>>>>>>> Intial commit for Spicy Veggie
+});
